2c89f0c57e63685e05b944da194c8070129117a6
Shaul Kimel <shaullk600@gmail.com>
2025-06-10 17:17:13 +0300

    Add File AccountToDB The database path is entered there in a smart way.

---
+++ AcountToDB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshinon
+{
+    internal static class AcountToDB
+    {
+        private static string PathDataBase;
+       
+        public static void UpdatePathDataBase(string value, string password)
+        {
+            if( ( !string.IsNullOrEmpty(value) ) && ( !string.IsNullOrEmpty(password) ) ) 
+            {
+                if (password ==  "1234")
+                {
+                    AcountToDB.PathDataBase = value;
+                }
+            }
+        }
+        public static string GetPathDataBase() 
+        { return PathDataBase; }
+    }
+}
