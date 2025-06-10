eb757b6bae1f3b47415a5a2af15103c6a92bfb0b
Shaul Kimel <shaullk600@gmail.com>
2025-06-10 09:24:50 +0300

    End of day. Beginning of day 10-06-25

--- Program.cs
+++ Program.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Malshinon;
+using Malshinon.Test;
+using Malshinon.Table_Pepole;
+using Malshinon.Menu;
+using MySql.Data.MySqlClient;
+
+
 namespace Malshinon
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
