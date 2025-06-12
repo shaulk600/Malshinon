6e4c1a112c688f879e004d7cb29b403089cbedf2
Shaul Kimel <shaullk600@gmail.com>
2025-06-12 15:06:43 +0300

    Update: AcountToDB.cs runtime errors were addressed - now becomes a singleton

--- AcountToDB.cs
+++ AcountToDB.cs
@@ -6,24 +6,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Malshinon.DAL;
+
 namespace Malshinon
 {
-    internal static class AcountToDB
+    internal class AcountToDB
     {
-        private static string PathDataBase;
+        /// <summary>
+        /// The class 'AccountToDB' is intended to provide a path to the database in order to retrieve data from it - it is not a static class.
+        /// The department is complete.
+        /// </summary>
 
-        public static void UpdatePathDataBase(string value, string password)
+        //Variable declaration
+        private string PathDataBase;
+        private static AcountToDB instance = null;
+
+        //constructor Private
+        private AcountToDB() {  }
+        // real constractor
+        public static AcountToDB GetInstance()
         {
-            if ((!string.IsNullOrEmpty(value)) && (!string.IsNullOrEmpty(password)))
+            if (instance == null)
             {
-                if (password == "1234")
+                instance = new AcountToDB();
+            }
+            return instance;
+        }
+
+
+        //methods
+
+        /// <summary>
+        /// The function should insert or update the path when accessing the DataBase
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="password"></param>
+        public void UpdatePathDataBase(string value, string password)
+        {
+            bool f = true;
+            while (f || (value != "" && password != "") )
+            {
+                if ((!string.IsNullOrEmpty(value)) && (!string.IsNullOrEmpty(password)))
                 {
-                    AcountToDB.PathDataBase = value;
+                    if (password == "1234")
+                    {
+                        //AcountToDB.PathDataBase = value; // אפשר לכתוב כך ואפשר כך
+                        if (value.Contains("server="))
+                        {
+                            this.PathDataBase = $"server=127.0.0.1;user=root;password=;database={value}";
+                        }
+                        else
+                        {
+                            this.PathDataBase = value;                           
+                        }
+                        f = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Enter a path value and password to access the system.");
                 }
             }
         }
-        public static string GetPathDataBase()
-        { return PathDataBase; }
+
+        /// <summary>
+        /// The function is supposed to return the path of the database in the project.
+        /// If no value was entered initially, the value returned will be "".
+        /// </summary>
+        /// <returns>String Path</returns>
+        //במקום לכתוב return או לשאול האם זה null אפשר לכתוב ככה
+        public string GetPathDataBase() => string.IsNullOrEmpty(PathDataBase) ? "" : PathDataBase;
+
     }
 }
 
